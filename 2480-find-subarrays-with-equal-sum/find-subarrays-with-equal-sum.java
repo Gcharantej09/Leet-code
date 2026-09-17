c23aaa58e8bd82c8dbcc611e2ebf100211d29673
g charan tej <gcharanyadav23@gmail.com>
2026-09-17 19:37:55 +0530

    Time: 2 ms (21.06%) | Memory: 43.4 MB (43.61%) - LeetSync

--- 2480-find-subarrays-with-equal-sum/find-subarrays-with-equal-sum.java
+++ 2480-find-subarrays-with-equal-sum/find-subarrays-with-equal-sum.java
@@ -2,8 +2,8 @@ class Solution {
     public boolean findSubarrays(int[] nums) {
         int sum[]=new int[nums.length-1];
         int j=0;
-        for(int i=0;i<=nums.length-2;i++ ){
-             sum[j]=nums[i]+nums[i+1];
+        for(int i=1;i<nums.length;i++ ){
+             sum[j]=nums[i-1]+nums[i];
                 j++;
         }
         for( int i=0;i<sum.length;i++){
